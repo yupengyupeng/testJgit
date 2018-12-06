719832326bd2832de84105dc0e0a024e6735bd8b
peng.yu <peng.yu@pay200.cn>
2018-12-06 16:06:34 +0800

    222

--- src/main/java/com/pay200/jgit/api/GetCommitMessage.java
+++ src/main/java/com/pay200/jgit/api/GetCommitMessage.java
@@ -17,11 +17,20 @@ package com.pay200.jgit.api;
  */
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.RenameDetector;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
 
 import com.pay200.jgit.helper.CookbookHelper;
 
@@ -30,19 +39,48 @@ import com.pay200.jgit.helper.CookbookHelper;
  */
 public class GetCommitMessage {
 
-    public static void main(String[] args) throws IOException {
-        try (Repository repository = CookbookHelper.openJGitCookbookRepository()) {
+    public static void main(String[] args) throws Exception {
+    	try (Repository repository = CookbookHelper.openJGitCookbookRepository()) {
             Ref head = repository.findRef("refs/heads/master");
             System.out.println("Found head: " + head);
-
             // a RevWalk allows to walk over commits based on some filtering that is defined
             try (RevWalk walk = new RevWalk(repository)) {
                 RevCommit commit = walk.parseCommit(head.getObjectId());
-
+                RevTree tree=commit.getTree();
+                System.out.println(tree.name());
+                
+              
+               // System.out.println(getLog(head.getObjectId()));
                 System.out.println("\nCommit-Message: " + commit.getFullMessage());
 
                 walk.dispose();
             }
         }
     }
+    public static List<DiffEntry> getLog() throws Exception {  
+    	Repository repository = CookbookHelper.openJGitCookbookRepository();
+    	Git git=new Git(repository);
+    	ObjectId mergeBase = repository.resolve("refs/heads/master");
+    	System.out.println(mergeBase.getName());
+    	git.pull().call();
+    	
+        Iterable<RevCommit> allCommitsLater = git.log().add(mergeBase).call();  
+        Iterator<RevCommit> iter = allCommitsLater.iterator();  
+        RevCommit commit = iter.next();  
+        TreeWalk tw = new TreeWalk(repository);  
+        tw.addTree(commit.getTree());  
+  
+        commit = iter.next();  
+        if (commit != null)  
+            tw.addTree(commit.getTree());  
+        else  
+            return null;  
+  
+        tw.setRecursive(true);  
+        RenameDetector rd = new RenameDetector(repository);  
+        rd.addAll(DiffEntry.scan(tw));  
+        System.out.println(rd.compute());
+        return rd.compute();  
+    }  
+    
 }
